d6a4f0f9d56d43d0490de086802d3a5e5baf76b0
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-09-15 17:55:46 +0600

    metadata

--- src/app/imageoptimization/page.jsx
+++ src/app/imageoptimization/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
-
+export const metadata={
+    title:"image"
+}
 const ImageOptimaization = () => {
     return (
         <div className="text-3xl text-center">
@@ -9,9 +11,15 @@ const ImageOptimaization = () => {
 
 
             <h1>image from Image tags</h1>
-            <Image  className="max-w-4xl mx-auto"  src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-            width={500}
-            height={600}></Image>
+            <Image
+  src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000"
+  width={600}
+  height={400}
+  alt="Optimized image"
+  placeholder="blur"
+  blurDataURL="data:image/jpeg;base64,/9j/2wBDAAgGBgcGBQg..." // tiny image string
+/>
+
         </div>
     );
 };
